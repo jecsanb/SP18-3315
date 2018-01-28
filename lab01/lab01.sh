c97493ef0f8c835e4fc137c6bc7837fb8aba9768
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-01-28 00:05:20 -0600

    done! double check with classmate later

---
+++ lab01/lab01.sh
@@ -0,0 +1,14 @@
+echo "=================="
+echo "cal
+      date"
+cal
+date '+%U %D %T'
+echo "=================="
+
+echo "=================="
+echo "uname -a"
+uname -a
+echo "=================="
+
+
+
