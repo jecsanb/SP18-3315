f2498563cb96a83f01199b8e93e2cabad75c7c97
Jecsan Blanco <jecsanb@outlook.com>
2018-02-04 16:37:26 -0600

    Quesiton 1 done

---
+++ lab02/files/lab02.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#
+# lab02.sh
+# Copyright (C) 2018 Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
+#
+# Distributed under terms of the MIT license.
+#
+echo "======== Qeustion 1 =========="
+echo "grep \"^[0-9][0-9][0-9]\"  hw02.txt "
+echo "=============================="
+grep "^[0-9][0-9][0-9]"  hw02.txt
+echo "=============================="
+echo " --------------------------- "
+echo "========= Question 2 ========="
+echo "diff file1.txt file2.txt"
+echo "=============================="
+diff file1.txt file2.txt
+
+echo "=============================="
+
+
+
