ffee1e068e8773a12751c420697ee907a7d5b374
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-02-27 19:22:06 -0600

    done

--- lab05/lab05.sh
+++ lab05/lab05.sh
@@ -3,8 +3,33 @@
 # lab05.sh
 # Copyright (C) 2018 Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
 #
-# Distributed under terms of the MIT license.
+# A very inefficient  way of caluating pi.
+# This program ueses the Monte Carlo method of generating pi.
 #
+# If a circle of radius R is inscribed inside a square with side length 2R,
+# then the area of the circle will be pi*R^2 and the area of the square will be (2R)^2.
+# So the ratio of the area of the circle to the area of the square will be pi/4.
+# This means that, if you pick N points at random inside the square, approximately N*pi/4 of those points should fall inside the circle.
+# This program picks points at random inside the square. It then checks to see if the point is
+# inside the circle (it knows it's inside the circle if x^2 + y^2 < R^2, where x and y are the coordinates of the point and R is the radius of the circle).
+#The program keeps track of how many points it's picked so far (pointsInSquare) and how many of those points fell inside the circle (inCircle).
 
-
+scale="scale=3";
+pointsInSquare=10000;
+#a way of gen numbers such that  0 < n < 1
+randMax=$(bc <<< "2^15 - 1");
+let inCircle=0;
+while ( pointsInSquare -gt 0 ); do
+    #gen the point
+    x=$(echo "$scale;$RANDOM/$randMax" | bc);
+    y=$(echo "$scale;$RANDOM/$randMax" | bc);
+    d=$(echo "$scale;sqrt ( $x^2 + $y^2 )" | bc);
+    #check if it falls in the 1 quadrant of the circle.
+    if [[ $(bc <<< "$d < 1") -eq 1 ]]; then
+        ((inCircle++));
+    fi
+    ((pointsInSquare--));
+done
+pi=$(bc -l <<< "4($inCircle/$pointsInSquare)");
+echo "The aproximation of pi using $pointsInSquare random points is $pi."
 
